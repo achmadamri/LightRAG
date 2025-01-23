655da08f84dcf5e51637b30fde56efc5973cc0e3
Achmad Amri <achmad.amri@gmail.com>
2025-01-23 10:59:21 +0700

    Update

--- examples/lightrag_ollama_demo.py
+++ examples/lightrag_ollama_demo.py
@@ -1,3 +1,4 @@
+import datetime
 import asyncio
 import os
 import inspect
@@ -5,6 +6,8 @@ import logging
 from lightrag import LightRAG, QueryParam
 from lightrag.llm import ollama_model_complete, ollama_embedding
 from lightrag.utils import EmbeddingFunc
+
+print("start time:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 WORKING_DIR = "./dickens"
 
@@ -16,7 +19,9 @@ if not os.path.exists(WORKING_DIR):
 rag = LightRAG(
     working_dir=WORKING_DIR,
     llm_model_func=ollama_model_complete,
-    llm_model_name="gemma2:2b",
+    # llm_model_name="gemma2:2b",
+    # llm_model_name="qwen2m",
+    llm_model_name="llama3.2",
     llm_model_max_async=4,
     llm_model_max_token_size=32768,
     llm_model_kwargs={"host": "http://localhost:11434", "options": {"num_ctx": 32768}},
@@ -29,25 +34,33 @@ rag = LightRAG(
     ),
 )
 
-with open("./book.txt", "r", encoding="utf-8") as f:
+with open("./data/posts_prod_sample_cleaned.csv", "r", encoding="utf-8") as f:
     rag.insert(f.read())
 
 # Perform naive search
+print("----------------------")
+print("naive")
 print(
     rag.query("What are the top themes in this story?", param=QueryParam(mode="naive"))
 )
 
 # Perform local search
+print("----------------------")
+print("local")
 print(
     rag.query("What are the top themes in this story?", param=QueryParam(mode="local"))
 )
 
 # Perform global search
+print("----------------------")
+print("global")
 print(
     rag.query("What are the top themes in this story?", param=QueryParam(mode="global"))
 )
 
 # Perform hybrid search
+print("----------------------")
+print("hybrid")
 print(
     rag.query("What are the top themes in this story?", param=QueryParam(mode="hybrid"))
 )
@@ -68,3 +81,5 @@ if inspect.isasyncgen(resp):
     asyncio.run(print_stream(resp))
 else:
     print(resp)
+
+print("end time:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
